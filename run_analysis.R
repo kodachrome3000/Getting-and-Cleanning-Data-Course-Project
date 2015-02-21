52d7d51ee92bb223feec7db39f8710054ab80bd1
kodachrome3000 <kodachrome3000@users.noreply.github.com>
2015-02-21 13:06:00 +1100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-setwd("/Users/nancyguo/R/Coursera/UCI Har Dataset")
-
 ## You should create one R script called run_analysis.R that does the following. 
 
 ## 1. Merges the training and the test sets to create one data set.
